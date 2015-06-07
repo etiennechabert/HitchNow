d24044d34abbc9589441705425bbecd287099e89
Etienne Chabert <etienne.chabert@gmail.com>
2015-06-07 02:33:41 +0200

    Base girl parsing passing

--- lib/girl_html_parser.rb
+++ lib/girl_html_parser.rb
@@ -1,9 +1,41 @@
 class GirlHtmlParser
-
     # @param [Nokogiri::HTML::Document] content
-    def self.analyse(content)
-        byebug
+    def initialize(html)
+        raise('WTF OBJECT ? Give me nokorigi or FU you') if html.class != Nokogiri::HTML::Document
+        @html = html
+    end
+
+    def analyse
+        analyse_profile_in_brief.merge(analyse_profile_details)
+    end
+
+    # @return [Hash]
+    def analyse_profile_in_brief
+        element = @html.css('#profile-in-brief')
         {
+            online: element.css('.last-cnx').text.strip == 'En ligne',
+            age: element.css('.age').text.strip,
+            city: element.css('.city').text.strip,
+            country: element.css('.country').text.strip,
+            id: element.css('#member-id').text.strip
+        }
+    end
+
+    def analyse_profile_details
+        element = @html.css('#view_details .data')
+        {
+            eyes: element.css('td')[0].children[1].children[2].text.strip,
+            profession: element.css('td')[1].children[1].children[2].text.strip,
+            hairs: element.css('td')[2].children[1].children[2].text.strip,
+            alcohol: element.css('td')[3].children[1].children[2].text.strip,
+            physics: element.css('td')[4].children[1].children[2].text.strip,
+            smoke: element.css('td')[5].children[1].children[2].text.strip,
+            style: element.css('td')[6].children[1].children[2].text.strip,
+            alimentation: element.css('td')[7].children[1].children[2].text.strip,
+            origins: element.css('td')[8].children[1].children[2].text.strip,
+            foodLikes: element.css('td')[9].children[1].children[2].text.strip,
+            hobbies: element.css('td')[10].children[1].children[2].text.strip,
+            particularities: element.css('td')[11].children[1].children[2].text.strip
         }
     end
 end
--- test/fixtures/girl_html_parser_test.rb
+++ test/fixtures/girl_html_parser_test.rb
@@ -8,28 +8,80 @@ class GirlHtmlParserTest < ActiveSupport::TestCase
         r
     end
 
+    def hash_included?(big, little)
+        return false unless little.keys.included_in? big.keys
+        return false unless little.values.included_in? big.values
+        true
+    end
+
     def base_girl_expected
         {
             online: true,
-            age: 22,
-            location: 'Courbevoie, France',
-            id: 110884558,
+            age: '22 ans',
+            city: 'Courbevoie',
+            country: 'France',
+            id: 'ID.110884558',
             eyes: 'marrons',
-            hairs: 'châtains, mi-longs, raides',
-            physics: '160 cm, 55 kg, normale',
-            style: 'classique, geekette',
-            origins: 'européennes',
-            hobbies: ' découvrir, lire, sortir',
             profession: 'étudiante en alternance',
+            hairs: 'châtains, mi-longs, raides',
             alcohol: 'de temps en temps',
+            physics: '160 cm, 55 kg, normale',
             smoke: 'tolère la fumée',
+            style: 'classique, geekette',
             alimentation: 'mange de tout',
+            origins: 'européennes',
             foodLikes: 'japonais, italien, chinois',
-            paricularities: 'non renseigné'
+            hobbies: 'découvrir, lire, sortir',
+            particularities: 'non renseigné'
         }
     end
 
-    test 'base girl parsing' do
-        assert_equal GirlHtmlParser.analyse(get_file_content), base_girl_expected
+    def base_girl_profile_in_brief
+        {
+            :online=>true,
+            :age=> '22 ans',
+            :city=> 'Courbevoie',
+            :country=> 'France',
+            :id=> 'ID.110884558'
+        }
     end
+
+    def base_girl_profile_data
+        {
+            :eyes=> 'marrons',
+            :profession=> 'étudiante en alternance',
+            :hairs=> 'châtains, mi-longs, raides',
+            :alcohol=> 'de temps en temps',
+            :physics=> '160 cm, 55 kg, normale',
+            :smoke=> 'tolère la fumée',
+            :style=> 'classique, geekette',
+            :alimentation=> 'mange de tout',
+            :origins=> 'européennes',
+            :foodLikes=> 'japonais, italien, chinois',
+            :hobbies=> 'découvrir, lire, sortir',
+            :particularities=> 'non renseigné'
+        }
+    end
+
+    test 'wrong argument' do
+        assert_raise RuntimeError do
+            GirlHtmlParser.new({})
+        end
+    end
+
+    test 'base girl parsing' do
+        girl_html_parser = GirlHtmlParser.new(get_file_content)
+        assert_equal girl_html_parser.analyse, base_girl_expected
+    end
+
+    test 'base girl parsing profile in brief' do
+        girl_html_parser = GirlHtmlParser.new(get_file_content)
+        assert_equal girl_html_parser.analyse_profile_in_brief, base_girl_profile_in_brief
+    end
+
+    test 'base girl parsing data' do
+        girl_html_parser = GirlHtmlParser.new(get_file_content)
+        assert_equal girl_html_parser.analyse_profile_details, base_girl_profile_data
+    end
+
 end
