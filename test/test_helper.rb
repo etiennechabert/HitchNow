82bebd87f7700ff5fd741e5441376511211e8703
Etienne Chabert <etienne.chabert@gmail.com>
2015-06-08 14:58:53 +0200

    Adding some generics content for test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,17 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def girl_file_content(file_name='base_girl_test.html')
+      f = File.open("test/fixtures/girls_html/#{file_name}")
+      r = Nokogiri::HTML(f)
+      f.close
+      r
+  end
+
+  def girl_parser(file_name='base_girl_test.html')
+    GirlHtmlParser.new(girl_file_content(file_name))
+  end
+
+
   # Add more helper methods to be used by all tests here...
 end
