b7dede8a78af23af8e4d259b03cb914edef969d7
Etienne Chabert <etienne.chabert@gmail.com>
2015-06-08 14:58:41 +0200

    Let's find some test ideas for n-n ?

---
+++ test/models/particularity_profile_relation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ParticularityProfileRelationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/style_profile_relation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StyleProfileRelationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
