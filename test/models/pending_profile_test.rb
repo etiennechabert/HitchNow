7330e22acee4e5db058b1c16979e382ecad6a74d
Etienne Chabert <etienne.chabert@gmail.com>
2015-06-08 14:57:40 +0200

    extend test on pending_profile

--- test/models/pending_profile_test.rb
+++ test/models/pending_profile_test.rb
@@ -26,6 +26,31 @@ class PendingProfileTest < ActiveSupport::TestCase
         assert_equal 3, PendingProfile.count
     end
 
+    test 'collection creation collision with profile' do
+        Profile.create(FactoryGirl.attributes_for(:profile, id: 42))
+        PendingProfile.create(id: 42)
+        PendingProfile.create_collection([42])
+        assert_equal PendingProfile.count, 0
+        PendingProfile.create_collection([21, 42])
+        assert_equal PendingProfile.count, 1
+    end
+
+    test 'collection creation from base girl page' do
+        parser = girl_parser
+        PendingProfile.create_collection(parser.profile_others_girls)
+        assert_equal(PendingProfile.count, parser.profile_others_girls.uniq.count)
+        PendingProfile.create_collection(parser.profile_others_girls)
+        assert_equal(PendingProfile.count, parser.profile_others_girls.uniq.count)
+    end
+
+    test 'pending profile is destroy after profile creation' do
+        parser = girl_parser
+        PendingProfile.create_collection(parser.profile_others_girls)
+        pending = PendingProfile.first
+        FactoryGirl.create(:profile, id: pending.id)
+        assert_not PendingProfile.find_by_id pending.id
+    end
+
     test 'link_generation' do
         link = PendingProfile.create({id: 42})
         assert_equal 'https://www.adopteunmec.com/profile/42', link.generate_link
